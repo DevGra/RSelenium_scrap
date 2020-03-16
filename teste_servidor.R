58f02e89d8128fea3c3342ac376eb25ec88c6eae
Carlos Graziani de Toledo <CGT@fapesp.local>
2020-03-16 10:58:34 -0300

    correção do acesso ao servidor

--- teste_servidor.R
+++ teste_servidor.R
@@ -1,34 +1,17 @@
-require(RSelenium)
+library(RSelenium)
 # tem que rodar via camando o codigo, ex p/ windows: java -jar selenium-server-standalone-x.xx.x.jar
+# exemplo de uso funcional
+# digitar no prompt para subir o servidor 
+# java -Dwebdriver.chrome.verboseLogging=true -Dwebdriver.chrome.driver=C:/Users/cgt/AppData/Local/binman/binman_chromedriver/win32/80.0.3987.106/chromeDriver.exe -jar selenium-server-standalone-4.0.0-alpha-2.jar -port 4444
+# cDrv <- chrome(port = 4444L, version = "80.0.3987.106")
 # após estar rodando o servidor será acessado pela porta: 4444
-remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4444L, browserName = "Chrome")
-remDr$open()
-remDr$getStatus()
 
-remDr$navigate('http://www.google.com/ncr')
-remDr$navigate('http://www.bbs.co.uk')
-remDr$getCurrentUrl()
 # --------------------------------------------
 
-library(wdman)
-library(RSelenium)
-driver <- rsDriver(browser("Chrome"))
-remDr <- driver[["client"]]
-remDr$open()
-
-#------------------------------------------------
-library(RSelenium)
-
-rD <- rsDriver(browser = "chrome", version = "latest", port = 4444L, chromever = "80.0.3987.106", verbose = TRUE, check = TRUE)
+rD <- rsDriver(browser = "chrome", version = "latest", port = 4444L, chromever = "latest", verbose = TRUE, check = FALSE)
 remDr <- rD$client
 remDr$navigate("https://google.com")
 
-# digitar no prompt para subir o servidor 
-# java -Dwebdriver.chrome.verboseLogging=true -Dwebdriver.chrome.driver=/chromeDriver.exe -jar selenium-server-standalone-3.9.0.jar -port 4444
-# cDrv <- chrome(port = 4444L, version = "80.0.3987.106")
-#  eCaps <- list(chromeOptions = list(
-#    args = c('--no-sandbox','--headless', '--disable-gpu', '--window-size=1280,800')
-#  ))
-#  remDr <- remoteDriver(browserName = "chrome", port = 4444L,
-#                       extraCapabilities = eCaps)
-# remDr$open()
+
+
+
